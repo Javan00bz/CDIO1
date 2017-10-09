6dbf85ce9bd8f8be5e0ca02c8d0e3d7d746d27b9
Zeydel <mads_seydel@yahoo.dk>
2017-10-09 15:35:48 +0200

    jaja

---
+++ 15_del1/src/spil/PlayGame.java
@@ -0,0 +1,36 @@
+package spil;
+
+public class PlayGame {
+	
+	public static void main(String[] args)
+	{
+		Player mads = new Player("Mads", 20);
+		Player jacob = new Player("Jacob", 0);
+		
+		System.out.println(mads.getName());
+		System.out.println(mads.getPoints());
+		mads.setName("Peter");
+		mads.setPoints(0);
+		System.out.println(mads.getName());
+		System.out.println(mads.getPoints());
+		System.out.println(mads.toString());
+		mads.rollDice();
+		System.out.println(mads.toString());
+		mads.rollDice();
+		mads.rollDice();
+		mads.rollDice();
+		mads.rollDice();
+		mads.rollDice();
+		mads.rollDice();
+		jacob.rollDice();
+		jacob.rollDice();
+		jacob.rollDice();
+		jacob.rollDice();
+		jacob.rollDice();
+		jacob.rollDice();
+		System.out.println(mads.toString());
+		System.out.println(jacob.toString());
+		
+	}
+
+}
