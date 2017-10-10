12de2b3ad7f35cbaabb449588a83ed5c790c2fb3
Alex <Alex@Alex-PC.eduroam.wireless.dtu.dk>
2017-10-10 16:55:40 +0200

    med de sidste tests

--- 15_del1/src/test/PlayGameTestTest.java
+++ 15_del1/src/test/PlayGameTestTest.java
@@ -26,19 +26,47 @@ public class PlayGameTestTest {
 	
 	
 	@Test
-	public void TestdicerollThousandtimes() {
+	public void TestDicerollThousandTimes() {
 		for(int o = 0;o<1000;o++) {
 		int low = 1;
 		int high = 6;
 		int actual = d1.roll();
-		System.out.print(actual);
+		//System.out.print(actual);
 		assertTrue("Error, random is too high", high >= actual);
 		assertTrue("Error, random is too low",  low  <= actual);
 	}
 		
+		
+	}
+
+	
+	@Test
+	public void TestRandomnessDice()
+	{
+		int amount12 = 0;
+		System.out.println("");
+		for(int o = 0 ; o<1000;o++) {
+			int a1 = d1.roll();
+			int a2 = d1.roll();
+			int actual = a1+a2;
+			if (actual == 12)
+				amount12++;
+		}
+		System.out.println(amount12);
+		assertTrue("Error, random is too high", 32 >= amount12);
+		assertTrue("Error, random is too low",  22  <= amount12);
 	}
 	
-
-
-
+		
+	public void TestResponseTid() {
+		long l = System.currentTimeMillis();
+		System.out.println(System.currentTimeMillis());
+		for(int o = 0 ; o<1000;o++) {
+			int actual = d1.roll();
+			}
+		long k = System.currentTimeMillis();
+		assertTrue(k-l<333000);
+		System.out.println(k-l);
+	}
 }
+
