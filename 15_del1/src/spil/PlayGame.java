fb1cabd18539e8c0149ef5cf1a72315b42d6049d
Alex <Alex@Alex-PC.eduroam.wireless.dtu.dk>
2017-10-10 11:48:52 +0200

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/Javan00bz/CDIO1.git
    
    Conflicts:
    	15_del1/src/spil/PlayGame.java

--- 15_del1/src/spil/PlayGame.java
+++ 15_del1/src/spil/PlayGame.java
@@ -1,11 +1,55 @@
 package spil;
 
+import desktop_resources.GUI;
+
 public class PlayGame {
 	
 	public static void main(String[] args)
 	{
 		Player mads = new Player("Mads", 20);
 		Player jacob = new Player("ToBiAz", 0);
+		
+		GUI.addPlayer("ayat", 500);
+		GUI.addPlayer("Alex", 500);
+		
+		int a;
+		int b;
+		
+		boolean qwe = true;
+		
+		while (qwe == true) {
+		GUI.setDice((int)(Math.random()*6+1),(int)(Math.random()*6+1));
+		//GUI.setDice(mads.rollDice(), mads.rollDice(), mads.rollDice(), mads.rollDice());
+		//GUI.setDice(3, 2, 1, 3, 2, 1);
+		//GUI.setDice(faceValue1, faceValue2);
+		GUI.getUserButtonPressed(" ", "ROLL DICE");
+		
+		a = (int)(Math.random()*6+1);
+		b = (int)(Math.random()*6+1);
+		GUI.setDice(a,b);
+		
+		
+		GUI.removeCar(a+b, "Ayat");
+		GUI.setCar(a+b, "Ayat");
+		
+		GUI.getUserButtonPressed(" ", "ROLL DICE");
+		
+		a = (int)(Math.random()*6+1);
+		b = (int)(Math.random()*6+1);
+		GUI.setDice(a, b);
+		
+		GUI.removeCar(a+b, "Alex");
+		GUI.setCar(a+b, "Alex");
+		
+		}
+		
+		GUI.getUserButtonPressed("ROLL", "ROLL DICE");
+		GUI.setDice((int)(Math.random()*6+1),(int)(Math.random()*6+1));
+		
+		GUI.getUserButtonPressed("ROLL", "ROLL DICE");
+		GUI.setDice((int)(Math.random()*6+1),(int)(Math.random()*6+1));
+		
+		
 		
 		
 		System.out.println(mads.getName());
